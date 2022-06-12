5c2383a2d76c6001294d89fddd4d90e1a60a361d
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-06-11 20:00:41 -0400

    finished example output

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer'); // import libraries needed
 const fs = require('fs');
 const html = require('./src/generateHTML.js');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 
 function writeToFile(fileName, data) { // write to file function
     fs.writeFile(`./dist/${fileName}.html`, data, (err) => {
@@ -11,10 +14,15 @@ function writeToFile(fileName, data) { // write to file function
 function init() { // main function to run the program
     if (process.argv[2] === 'test' || process.argv[2] === 'example') { // run example 
         const example = {
-
+            manager: new Manager('Janice', 1, 'janice@company.com', 1),
+            eng1: new Engineer('Brad', 2, 'brad@company.com', 'ItzBrad'),
+            eng2: new Engineer('Chad', 3, 'chad@company.com', 'chadscode'),
+            eng3: new Engineer('Tad', 4, 'tad@company.com', 'typertad'),
+            intern: new Intern('Jerry', 5, 'jerry@company.com', 'MIT')
         }
 
-        writeToFile('example', example);
+        writeToFile('example', html(example));
+        return;
     }
 }
 
